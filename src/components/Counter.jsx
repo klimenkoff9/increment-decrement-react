6199bfd73b2bf92eabfa46d544ca1455bfb765b9
klimenkoff9 <dennis.klimenkov9@gmail.com>
2021-01-16 21:11:35 -0500

    Upload finished project

---
+++ src/components/Counter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.count}
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
